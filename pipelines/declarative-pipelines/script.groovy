e6e99f256747769b6831a1d9ef9c9124ea86b88b
avielb <aviel33@gmail.com>
2019-07-13 15:34:42 +0300

    declarative pipes

---
+++ pipelines/declarative-pipelines/script.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+    stages {
+        stage('Example') {
+            steps {
+                echo 'Hello World'
+
+                script {
+                    def browsers = ['chrome', 'firefox']
+                    for (int i = 0; i < browsers.size(); ++i) {
+                        echo "Testing the ${browsers[i]} browser"
+                    }
+                }
+            }
+        }
+    }
+}
