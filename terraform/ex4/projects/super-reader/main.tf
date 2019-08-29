f019d8d1ed4adbe33341e8a5d504e2bedfdcfeef
avielb <aviel33@gmail.com>
2019-08-29 09:10:08 +0300

    add provisioner for loading data

--- terraform/ex4/projects/super-reader/main.tf
+++ terraform/ex4/projects/super-reader/main.tf
@@ -23,3 +23,9 @@ module "ec2" {
   cluster-name = "super-reader"
   user_data = local.user_data
 }
+
+resource "null_resource" "load_data" {
+  provisioner "local-exec" {
+    command = " aws dynamodb batch-write-item --request-items file:///Users/avielb/GitHub/advanced-devops/terraform/ex4/projects/super-reader/ProductCatalog.jsontf des"
+  }
+}
