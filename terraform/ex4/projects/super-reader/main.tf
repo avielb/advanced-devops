7cef6127cd2f3066b2467f773b88b8e5ca53f33c
avielb <aviel33@gmail.com>
2019-09-03 08:26:31 +0300

    fix typo

--- terraform/ex4/projects/super-reader/main.tf
+++ terraform/ex4/projects/super-reader/main.tf
@@ -26,6 +26,6 @@ module "ec2" {
 
 resource "null_resource" "load_data" {
   provisioner "local-exec" {
-    command = " aws dynamodb batch-write-item --request-items file:///Users/avielb/GitHub/advanced-devops/terraform/ex4/projects/super-reader/ProductCatalog.jsontf des"
+    command = " aws dynamodb batch-write-item --request-items file:///Users/avielb/GitHub/advanced-devops/terraform/ex4/projects/super-reader/ProductCatalog.json"
   }
 }
