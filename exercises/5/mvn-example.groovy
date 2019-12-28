cc7f28a38bc5a21c2a3e3ec1849cb2ec4f4656e5
avielb <aviel33@gmail.com>
2019-12-28 10:00:35 +0200

    add exercise 5 codes

--- exercises/5/mvn-example.groovy
+++ exercises/5/mvn-example.groovy
@@ -1,6 +1,6 @@
 def label = "mypod-${UUID.randomUUID().toString()}"
 podTemplate(label: label, containers: [
-		containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true)]) {
+		containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: '/bin/sh')]) {
 	node(label) {
 		stage("Checkout") {
 			git "https://github.com/web3j/sample-project-maven.git"
