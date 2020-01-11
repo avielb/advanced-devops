ce7a736369662e2bc170378b7dead0f285bf780d
avielb <aviel33@gmail.com>
2020-01-11 11:09:28 +0200

    add exercise 7 answers + more commands.txt

---
+++ exercises/7/ec2.tf
@@ -0,0 +1,18 @@
+locals {
+  user_data = <<EOF
+#!/bin/bash
+yum install docker -y
+systemctl enable docker
+systemctl start docker
+docker run -d -p 80:80 --name dokuwiki bitnami/dokuwiki:latest
+EOF
+}
+
+resource "aws_instance" "dokuwiki" {
+  ami                    = "ami-0b898040803850657"
+  key_name               = "DevOps"
+  instance_type          = "t2.micro"
+  user_data              = base64encode(local.user_data)
+  vpc_security_group_ids = [aws_security_group.servers-sg.id, data.aws_security_group.default-sg.id]
+  depends_on             = [aws_security_group.servers-sg]
+}
