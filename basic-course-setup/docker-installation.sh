153fb4778ea9ec58bc555a077c4f05c1fe150b52
avielb <aviel33@gmail.com>
2021-03-02 07:34:32 +0200

    add more stuff to basic course

---
+++ basic-course-setup/docker-installation.sh
@@ -0,0 +1,5 @@
+yum install docker -y
+systemctl enable docker
+systemctl start docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
