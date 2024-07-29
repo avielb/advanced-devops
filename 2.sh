0e7d0668f22b0df4be8cb658237ba7797903ca7d
Aviel Buskila <aviel33@gmail.com>
2024-07-29 19:59:26 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,3 +5,7 @@ docker rm -vf my-nginx
 docker run -d -p 8082:80 --name my-nginx2 nginx:1.26-alpine
 docker build -t test .
 docker run -d --name moshe test
+
+docker network create my-network
+docker run -d --name nginx1 --network my-network nginx:alpine
+docker run -d --name nginx2 --network my-network nginx:alpine
