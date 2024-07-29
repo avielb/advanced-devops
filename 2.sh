71af73ad0917229ec31af89c952d087f4b92d8bf
Aviel Buskila <aviel33@gmail.com>
2024-07-29 19:30:42 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -3,3 +3,4 @@ docker run -d -p 8080:80 --name my-nginx nginx:alpine
 docker exec -it my-nginx sh
 docker rm -vf my-nginx
 docker run -d -p 8082:80 --name my-nginx2 nginx:1.26-alpine
+docker build -t test .
