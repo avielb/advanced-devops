68f366e1dff6996e388f0b27826d9083586dea45
Aviel Buskila <aviel33@gmail.com>
2024-08-05 19:09:36 +0300

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -6,3 +6,4 @@ kubectl create deployment hello-node --image=nginx:alpine --replicas=5
 kubectl scale deploy/hello-node --replicas=10
 kubectl set image deploy/hello-node nginx=nginx:16-alpine
 kubectl rollout undo deployment/hello-node --to-revision=1
+kubectl set image deploy/hello-node nginx=nginx:1.16-alpine
