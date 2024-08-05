cbee1167e9b8b90930048efed4c565c81c75e20a
Aviel Buskila <aviel33@gmail.com>
2024-08-05 18:54:23 +0300

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -2,3 +2,4 @@ kubectl get pods
 kubectl run nginx --image=nginx:1.15.12-alpine
 kubectl run redis --image=redis:5.0.4-alpine
 kubectl logs redis
+kubectl create deployment hello-node --image=nginx:alpine --replicas=5
