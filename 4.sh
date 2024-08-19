2827c0ce00a8e7cc1de24127004e4118f5295870
Aviel Buskila <aviel33@gmail.com>
2024-08-19 18:38:42 +0300

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,5 @@
 kubectl get storageclass
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/master/volumes/pvc.yaml
 kubectl get pvc
+kubectl get pv
+kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/master/volumes/pod-with-pvc.yaml
