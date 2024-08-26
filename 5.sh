23dbb0f526c3bca2d8861491251267cdcce2c9c7
Aviel Buskila <aviel33@gmail.com>
2024-08-26 18:34:50 +0300

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -3,5 +3,11 @@ docker exec -it master bash
 apt update && apt install python3-pip vim git -y
 rm -rf /usr/lib/python3.12/EXTERNALLY-MANAGED
 pip install ansible
+
+# for mac run
 docker run --name node1 avielb/ansible-demo:arm64 
 docker run --name node2 avielb/ansible-demo:arm64 
+
+# for windows run
+docker run --name node1 avielb/ansible-demo:amd64 
+docker run --name node2 avielb/ansible-demo:amd64 
