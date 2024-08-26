481d284933ec02606b07cdd53f749d13eeaffe15
Aviel Buskila <aviel33@gmail.com>
2024-08-26 20:07:13 +0300

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -2,11 +2,14 @@ git clone https://github.com/avielb/ansible-demo
 cd ansible-demo
 docker compose up -d
 docker exec -it ansible-demo-ansible-1 bash
+ssh-keygen
 ssh-copy-id node1
+screencast
 ssh-copy-id node2
 screencast
 
-
+git clone https://github.com/avielb/ansible-demo
+cd ansible-demo
 ansible servers -m ping -i hosts
 
 ansible servers -m shell -a "echo hello world" -i hosts
@@ -14,5 +17,3 @@ ansible servers -m apt -a "name=nginx state=present" -i hosts
 ansible-playbook vars.yml -i hosts
 ansible-playbook vars.yml -i hosts --tags=tag1
 ansible-playbook demo.yml -i hosts
-git clone https://github.com/avielb/ansible-demo
-cd ansible-demo
