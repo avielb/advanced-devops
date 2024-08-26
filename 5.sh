1130c462410f147184ae0effbfacd8bb5945e211
Aviel Buskila <aviel33@gmail.com>
2024-08-26 19:38:39 +0300

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -13,3 +13,4 @@ ansible servers -m shell -a "echo hello world" -i hosts
 ansible servers -m apt -a "name=nginx state=present" -i hosts
 ansible-playbook vars.yml -i hosts
 ansible-playbook vars.yml -i hosts --tags=tag1
+ansible-playbook demo.yml -i hosts
