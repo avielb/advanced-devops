2ad8fe785ccc811c1adf7cb65d87ff99b8e5eed3
Aviel Buskila <aviel33@gmail.com>
2024-09-16 18:33:23 +0300

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -8,3 +8,9 @@ provider "helm" {
   }
 }
 https://registry.terraform.io/modules/terraform-iaac/nginx-controller/helm/latest?tab=inputs
+
+module "app" {
+  source = "../../modules/app"
+  env = "prod"
+  controller_kind = "Deployment" # use this
+}
