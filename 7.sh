f9a4c33f94e555311221d80177dc8e12b51dd28e
Aviel Buskila <aviel33@gmail.com>
2024-10-31 20:33:58 +0200

    Update 7.sh

--- 7.sh
+++ 7.sh
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
+data = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+]
 @app.route('/')
 def home():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/names')
+def names():
+    return jsonify(data)
+app.run()
