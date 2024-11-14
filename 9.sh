f91765889210c96bb93463994d42ed9cb835c08e
Aviel Buskila <aviel33@gmail.com>
2024-11-14 18:21:37 +0200

    Update 9.sh

--- 9.sh
+++ 9.sh
@@ -2,3 +2,4 @@ docker run -d --name gitlesson  ubuntu:latest sleep 40000000
 docker exec -it gitlesson bash
 apt update && apt install -y git
 mkdir devops && cd devops
+mkdir 1909-first-repo && cd 1909-first-repo
