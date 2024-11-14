c333d4f264779d0703d5c8e137629831e4a25d7f
Aviel Buskila <aviel33@gmail.com>
2024-11-14 20:26:30 +0200

    Update 9.sh

--- 9.sh
+++ 9.sh
@@ -10,3 +10,4 @@ git commit -am "init commit"
 cd ~/devops && mkdir branches && cd branches
 echo 1 > 1.txt 
 git add . && git commit -am "init commit"
+git clone https://github.com/avielb/advanced-devops.git
