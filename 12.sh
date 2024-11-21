11718a79384086b03c55610ef8c23c1d3b2a0677
Aviel Buskila <aviel33@gmail.com>
2024-11-21 20:33:49 +0200

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -7,3 +7,23 @@ https://github.com/new
 https://github.com/settings/tokens/new
 * * * * *
 .github/workflows/first.yaml
+
+
+
+name: List Files on Main Commit
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  list-files:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v3
+
+    - name: Run ls -l
+      run: ls -l
