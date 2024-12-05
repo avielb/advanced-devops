f510ad54c2ce9c8ee795d17ed044bf3ce759170e
Aviel Buskila <aviel33@gmail.com>
2024-12-05 18:22:39 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,1 @@
-docker rm -vf my-ubuntu
-docker run -d --name  my-ubuntu ubuntu:latest sleep 1000000
-docker exec -it my-ubuntu bash
-
-apt update
-apt install less -y
+docker run -d -p 8080:80 nginx:alpine
