cfafcf553b64c7434660b4e7cfb742259b27bff7
Aviel Buskila <aviel33@gmail.com>
2024-12-12 18:53:55 +0200

    Update 13.sh

--- 13.sh
+++ 13.sh
@@ -4,3 +4,4 @@ kubectl run example-pod --image=nginx:alpine --restart=Never
 kubectl describe pod example-pod
 kubectl create deployment example-deployment --image=nginx:alpine --replicas=3
 kubectl describe deployment example-deployment
+kubectl scale deployment example-deployment --replicas=10
