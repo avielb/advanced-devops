11a2f252e86b47d6bbebab0e54c80134b34cf60b
Aviel Buskila <aviel33@gmail.com>
2024-12-19 18:50:51 +0200

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -1,3 +1,5 @@
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pv.yaml
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pvc.yaml
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl exec -it task-pv-pod -- bash
+cd /usr/share/nginx/html
