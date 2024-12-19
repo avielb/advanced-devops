ced58681fd94d0ce91f7fc43fd98ac4c547b178e
Aviel Buskila <aviel33@gmail.com>
2024-12-19 18:28:58 +0200

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -1,1 +1,2 @@
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pv.yaml
+kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pvc.yaml
