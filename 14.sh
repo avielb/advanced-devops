0e3d51ae04d60e2eda3275ac6fbb6a7a539ab5dd
Aviel Buskila <aviel33@gmail.com>
2024-12-19 19:43:23 +0200

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -10,3 +10,4 @@ git clone https://github.com/avielb/k8s-demo.git
 cd k8s-demo/volumes
 kubectl create configmap game-config --from-file=configs/
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-config-map.yaml
+kubectl exec -it pod-env-var -- bash
