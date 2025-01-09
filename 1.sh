b3d19e04a3cb3072cf9dcfeabb68c8ede69a0115
Aviel Buskila <aviel33@gmail.com>
2025-01-09 18:53:51 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,3 +4,5 @@ systemctl enable docker
 systemctl start docker
 docker run -d -p 80:80 nginx:alpine
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
