6b1c5e127d5e8b249e90723c2d6d7493258d5c9e
Aviel Buskila <aviel33@gmail.com>
2025-01-09 18:54:54 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -6,3 +6,4 @@ docker run -d -p 80:80 nginx:alpine
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
+kind create cluster
