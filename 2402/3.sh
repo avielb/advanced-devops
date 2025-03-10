01b73ee296ec0d004157454138c2a2ab293bcbd2
Aviel Buskila <aviel33@gmail.com>
2025-03-10 19:01:11 +0200

    Update 3.sh

--- 2402/3.sh
+++ 2402/3.sh
@@ -6,3 +6,4 @@ kubectl create deployment hello-node --image=nginx:alpine --replicas=5
 kubectl delete pod nginx
 kubectl scale deployment hello-node --replicas=20
 kubectl set image deploy/hello-node nginx=nginx:latest
+kubectl describe deployment hello-node
