b142cda084153dadc98cbfecb968785ec84d2a46
Aviel Buskila <aviel33@gmail.com>
2025-03-17 18:31:20 +0200

    Update 4.sh

--- 2402/4.sh
+++ 2402/4.sh
@@ -1,2 +1,3 @@
 kubectl get pods 
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pv.yaml
+kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pvc.yaml
