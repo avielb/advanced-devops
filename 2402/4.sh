18b85e2cb13ae0108157d84c423aca084ebd238f
Aviel Buskila <aviel33@gmail.com>
2025-03-17 18:48:15 +0200

    Update 4.sh

--- 2402/4.sh
+++ 2402/4.sh
@@ -3,3 +3,7 @@ kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/ma
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pvc.yaml
 kubectl describe persistentvolumeclaim/task-pv-claim
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl exec -it task-pv-pod -- bash
+cd usr/share/nginx/html/
+echo 1 > index.html
+exit
