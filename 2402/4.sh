c1d02d1ec5b8de2b2864f6ac95168a5e41a80d2d
Aviel Buskila <aviel33@gmail.com>
2025-03-17 19:02:02 +0200

    Update 4.sh

--- 2402/4.sh
+++ 2402/4.sh
@@ -8,3 +8,5 @@ cd usr/share/nginx/html/
 echo 1 > index.html
 exit
 kubectl debug node/docker-desktop -it --image=busybox -- sh
+kubectl delete -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
