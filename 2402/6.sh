fb26c98d5d180f6f29ec2e4dba91a4bc4224a4cc
Aviel Buskila <aviel33@gmail.com>
2025-04-21 18:59:13 +0300

    Update 6.sh

--- 2402/6.sh
+++ 2402/6.sh
@@ -1,3 +1,8 @@
 git init
 git config --global user.name "Your Name"
 git config --global user.email "you@example.com"
+for macs with another user 
+git credential-osxkeychain erase
+host=github.com
+protocol=https
+[Press Return]
