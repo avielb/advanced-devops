68cb310b401bcdb3d019c9f54fbdf61a4af1d003
Aviel Buskila <aviel33@gmail.com>
2025-04-28 18:22:09 +0300

    Update 7.sh

--- 2402/7.sh
+++ 2402/7.sh
@@ -1,1 +1,24 @@
 # make sure to work with a github repo before class in pycharm
+mkdir .github/workflows
+cd .github/workflows
+create a file: 01-example.yaml
+name: Run Tests
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v3
+      - name: show files
+        run: |
+          ls -ltr
