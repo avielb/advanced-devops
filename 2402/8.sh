a2851cd77792c63b19e6136aa3f9b761c7264904
Aviel Buskila <aviel33@gmail.com>
2025-05-05 18:22:19 +0300

    Update 8.sh

--- 2402/8.sh
+++ 2402/8.sh
@@ -2,3 +2,5 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl get all -n argocd
+
+kubectl get applications -A
