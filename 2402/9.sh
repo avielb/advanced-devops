c71416ccfef086b17138e351e005850b29eb6fe6
Aviel Buskila <aviel33@gmail.com>
2025-05-12 20:34:56 +0300

    Update 9.sh

--- 2402/9.sh
+++ 2402/9.sh
@@ -5,3 +5,4 @@ node_network_receive_bytes_total{device='eth0'}
 rate(node_network_receive_bytes_total{device='eth0'}[5m])
 job_name: kubernetes-nodes
 http://prometheus-server
+kubectl apply -f nginx.yaml
