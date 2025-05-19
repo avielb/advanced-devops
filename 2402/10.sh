82a23b9833494c05f74c71e4f44295e918f45b58
Aviel Buskila <aviel33@gmail.com>
2025-05-19 20:17:29 +0300

    Update 10.sh

--- 2402/10.sh
+++ 2402/10.sh
@@ -25,3 +25,12 @@ AmazonEC2FullAccess
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0953476d60561c955" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
