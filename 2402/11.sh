e1391d801634df8a6d4707c2030f4359ec575999
Aviel Buskila <aviel33@gmail.com>
2025-05-26 18:23:14 +0300

    Update 11.sh

--- 2402/11.sh
+++ 2402/11.sh
@@ -3,17 +3,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami = "ami-05c13eab67c5d8861"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-018523f5b12a8b3fb"
-  tags = {
-    Name = var.server_name
-  }
-}
+resource "null_resource" "example" {}
 
 variable "server_name" {
   type = string
-  default = "my-server"
+  default = "moshe"
   description = "the name of the server"
+}
+
+output "name" {
+  value = var.server_name
 }
