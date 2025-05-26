1c5f67aa2425528dadea1892c3dde1d39953b50c
Aviel Buskila <aviel33@gmail.com>
2025-05-26 20:39:16 +0300

    Update 11.sh

--- 2402/11.sh
+++ 2402/11.sh
@@ -156,11 +156,18 @@ volumes:
 in class11/dev/localstack/main.tf
 ---
 provider "aws" {
-  region     = "us-east-1"
+
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region                      = "us-east-1"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   endpoints {
-    s3 = "http://localhost:4566"
-    iam = "http://localhost:4566"
+    s3             = "http://s3.localhost.localstack.cloud:4566"
   }
 }
 
