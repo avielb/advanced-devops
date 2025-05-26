0a317ed2a8d2a9dfce4824f23b12c7c18d66c29f
Aviel Buskila <aviel33@gmail.com>
2025-05-26 18:14:18 +0300

    Update 11.sh

--- 2402/11.sh
+++ 2402/11.sh
@@ -1,1 +1,19 @@
 # create folder in the pycharm project called "class11"
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-05c13eab67c5d8861"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-018523f5b12a8b3fb"
+  tags = {
+    Name = var.server_name
+  }
+}
+
+variable "server_name" {
+  type = string
+  default = "my-server"
+  description = "the name of the server"
+}
