7c62319a856abbef3826692ee1843985b6b33992
Aviel Buskila <aviel33@gmail.com>
2025-06-09 20:17:38 +0300

    Update 12.sh

--- 2402/12.sh
+++ 2402/12.sh
@@ -1,9 +1,11 @@
 import json
+import os
 
 def lambda_handler(event, context):
     # TODO implement
     print("moshe")
     print(event)
+    print(os.environ)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!'),
