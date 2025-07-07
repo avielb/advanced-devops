748e8c98387072572bcc21738450387a8c05b448
Aviel Buskila <aviel33@gmail.com>
2025-07-07 18:21:55 +0300

    Update 14.sh

--- 2402/14.sh
+++ 2402/14.sh
@@ -2,3 +2,5 @@ git clone https://github.com/avielb/ansible-demo
 cd ansible-demo
 docker compose up -d
 docker exec -it ansible-demo-ansible-1 bash
+git clone https://github.com/avielb/ansible-demo
+cd ansible-demo
