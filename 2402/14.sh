9079b4ede61622df2041647759e65c21c4503612
Aviel Buskila <aviel33@gmail.com>
2025-07-07 20:16:55 +0300

    Update 14.sh

--- 2402/14.sh
+++ 2402/14.sh
@@ -25,3 +25,14 @@ ssh node2 cat /root/hostname.conf
 cd roles/common/
 mkdir defaults 
 echo -e "---\nvar1: haim" >  defaults/main.yml
+
+# edit common.yml
+---
+- hosts: servers
+
+  roles:
+    - common
+  tasks:
+    - name: show
+      debug:
+        msg: "var1 is: {{var1}}"
