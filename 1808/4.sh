15dbcee3f3670d9513ae475082b4a6229ddc1bc7
Aviel Buskila <aviel33@gmail.com>
2025-09-08 20:35:35 +0300

    Update 4.sh

--- 1808/4.sh
+++ 1808/4.sh
@@ -23,5 +23,5 @@ kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/ma
 kubectl delete -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/statefulset.yaml
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/statefulset.yaml
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/daemonset.yaml
-
+kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/cronjob.yaml
 kubectl scale sts/postgres --replicas=5
