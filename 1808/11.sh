ad708595c1a80a74c2cea97ff6a884976074adb2
Aviel Buskila <aviel33@gmail.com>
2025-11-24 19:21:45 +0200

    Update 11.sh

--- 1808/11.sh
+++ 1808/11.sh
@@ -44,6 +44,16 @@ output "name" {
 --------------------------------------------------
 variable "var1" {}
 variable "var2" {}
+variable "environment" {
+  default = "prod"
+}
+locals {
+  names = "${var.var1}_${var.environment}"
+}
+output "ss" {
+  value = local.names
+}
+# module.myapp.null_resource.example
 -------------------------------------------
 output "result" {
   value = "${var.var1} --- ${var.var2}"
